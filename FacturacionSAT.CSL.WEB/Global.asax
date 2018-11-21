ecfa47019d787fea3c1d6b6739834ec559e067ac
EnriqueMB <kikeballina1@gmail.com>
2018-11-21 16:04:44 -0600

    Cambios Enrique Mendez Ballina

---
+++ FacturacionSAT.CSL.WEB/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FacturacionSAT.CSL.WEB.MvcApplication" Language="C#" %>
---
+++ FacturacionSAT.CSL.WEB/Global.asax.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.Http;
+
+namespace FacturacionSAT.CSL.WEB
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
