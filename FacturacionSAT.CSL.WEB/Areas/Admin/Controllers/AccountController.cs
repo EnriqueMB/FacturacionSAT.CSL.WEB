15bb40648ea7b572bdf41ab20c1190fd1c7c68f7
EnriqueMB <kikeballina1@gmail.com>
2018-11-22 13:44:08 -0600

    asdsadasd

--- FacturacionSAT.CSL.WEB/Areas/Admin/Controllers/AccountController.cs
+++ FacturacionSAT.CSL.WEB/Areas/Admin/Controllers/AccountController.cs
@@ -1,9 +1,11 @@
 ﻿using FacturacionSAT.CSL.WEB.Filters;
+using FacturacionSAT.CSL.WEB.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace FacturacionSAT.CSL.WEB.Areas.Admin.Controllers
 {
@@ -12,9 +14,123 @@ namespace FacturacionSAT.CSL.WEB.Areas.Admin.Controllers
     public class AccountController : Controller
     {
         // GET: Areas/Account
+        [AllowAnonymous]
         public ActionResult Index()
         {
-            return View();
+            try
+            {
+                FormsAuthentication.SignOut();
+                if (User.Identity.IsAuthenticated)
+                {
+                    //UsuarioModels usuario = new UsuarioModels();
+                    //_Usuario_Datos UsuarioDatos = new _Usuario_Datos();
+                    //usuario.conexion = Conexion;
+                    //usuario.cuenta = User.Identity.Name;
+
+                    int TipoUsario = 1;//UsuarioDatos.ObtenerTipoUsuarioByUserName(usuario);
+                    if (TipoUsario == 1)
+                    {
+                        return RedirectToAction("Index", "HomeAdmin", new { Area = "Admin" });
+                    }
+                   
+                    else
+                        return RedirectToAction("LogOff", "Account");
+                }
+                else
+                    return View();
+            }
+            catch (Exception)
+            {
+                return RedirectToAction("LogOff", "Account");
+            }
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [OutputCache(NoStore = true, Duration = 0, VaryByParam = "None")]
+        public ActionResult Index(UsuarioModels model, string returnUrl)
+        {
+            FormsAuthentication.SetAuthCookie("1", model.RememberMe);
+            return RedirectToAction("Index", "HomeAdmin", new { Area = "Admin" });
+            //LoginDatos UD = new LoginDatos();
+            //model.conexion = Conexion;
+            //model = UD.ValidarUsuario(model);
+            //if (model.opcion == 1)
+            //{
+            //    FormsAuthentication.SignOut();
+            //    _Usuario_Datos usuario_datos = new _Usuario_Datos();
+            //    UsuarioModels usuario = new UsuarioModels();
+            //    //usuario.conexion = Conexion;
+            //    //usuario.cuenta = model.id_usuario;
+            //    int TipoUsario = usuario_datos.ObtenerTipoUsuarioByUserName(usuario);
+            //    System.Web.HttpContext.Current.Session["SessionTipoUsuario"] = TipoUsario;
+            //    FormsAuthentication.SetAuthCookie(model.id_usuario, model.RememberMe);
+            //    HttpCookie authCookie = FormsAuthentication.GetAuthCookie(model.id_usuario, model.RememberMe);
+            //    FormsAuthenticationTicket ticket = FormsAuthentication.Decrypt(authCookie.Value);
+            //    List<string> listaPermiso = new List<string>();
+            //    foreach (var item in model.ListaPermisos)
+            //    {
+            //        listaPermiso.Add(item.NombreUrl);
+            //    }
+            //    System.Web.HttpContext.Current.Session["SessionListaPermiso"] = listaPermiso;
+            //    System.Web.HttpContext.Current.Session["NombreUsuario"] = model.nombreCompleto;
+            //    if (TipoUsario == 1)
+            //    {
+            //        return RedirectToAction("Index", "HomeAdmin", new { Area = "Admin" });
+            //    }
+            //    //else if (id_tipoUsuario == "1")
+            //    //{
+            //    //    return RedirectToAction("Index", "HomeProfesor", new { Area = "Profesor" });
+            //    //}
+            //    else
+            //    {
+            //        ModelState.AddModelError("", "No tienes permisos");
+            //        Session.Abandon();
+            //        Session.Clear();
+            //        Session.RemoveAll();
+            //        return View(model);
+            //    }
+            //}
+            //else if (model.opcion == 2)
+            //{
+            //    ModelState.AddModelError("", "Usuario no existe");
+            //    Session.Abandon();
+            //    Session.Clear();
+            //    Session.RemoveAll();
+            //    return View(model);
+            //}
+            //else if (model.opcion == 3)
+            //{
+            //    ModelState.AddModelError("", "Error de Contraseña");
+            //    Session.Abandon();
+            //    Session.Clear();
+            //    Session.RemoveAll();
+            //    return View(model);
+            //}
+            //else if (model.opcion == 4)
+            //{
+            //    ModelState.AddModelError("", "El usuario tiene que ser de tipo. Administrador");
+            //    Session.Abandon();
+            //    Session.Clear();
+            //    Session.RemoveAll();
+            //    return View(model);
+            //}
+            //else
+            //{
+            //    ModelState.AddModelError("", "El usuario o contraseña son incorrectos!!.");
+            //    Session.Abandon();
+            //    Session.Clear();
+            //    Session.RemoveAll();
+            //    return View(model);
+            //}
+        }
+
+        // GET: /Account/LogOff
+        [AllowAnonymous]
+        public ActionResult LogOff()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Account");
         }
     }
 }
---
+++ FacturacionSAT.CSL.WEB/Models/UsuarioModels.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FacturacionSAT.CSL.WEB.Models
+{
+    public class UsuarioModels
+    {
+        public string user { get; set; }
+        public string Password { get; set; }
+        public bool RememberMe { get; set; }
+
+    }
+}
