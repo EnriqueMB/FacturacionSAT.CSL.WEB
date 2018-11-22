564b39d3fa3bec2ff4195a370f5f9ebabb8ede5a
CreativaSL <CreativaSL@CREATIVASL-PC>
2018-11-22 15:46:56 -0600

    ok fgg

---
+++ FacturacionSAT.CSL.WEB/Models/ComboModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FacturacionSAT.CSL.WEB.Models
+{
+    public class ComboModel
+    {
+        public string Id { get; set; }
+        public string Value { get; set; }
+    }
+}
---
+++ FacturacionSAT.CSL.WEB/Models/Datos/ComboDatos.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using Microsoft.ApplicationBlocks.Data;
+
+namespace FacturacionSAT.CSL.WEB.Models.Datos
+{
+    public class ComboDatos
+    {
+        public List<ComboModel> ListaEmisores(AuxSQLModel oAuxSQLModel)
+        {
+            try
+            {
+                List<ComboModel> lista = new List<ComboModel>();
+                ComboModel item;
+                SqlDataReader dr = null;
+                dr = SqlHelper.ExecuteReader(oAuxSQLModel.Conexion, "[dbo].[CFDIDatosEmisorV2_Combo_sp]");
+                while (dr.Read())
+                {
+                    item = new ComboModel();
+                    item.Id = !dr.IsDBNull(dr.GetOrdinal("id")) ? dr.GetString(dr.GetOrdinal("id")) : string.Empty;
+                    item.Value = !dr.IsDBNull(dr.GetOrdinal("value")) ? dr.GetString(dr.GetOrdinal("value")) : string.Empty;
+                    lista.Add(item);
+                }
+                dr.Close();
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public List<ComboModel> ListaFormaPagoDetalle(AuxSQLModel oAuxSQLModel)
+        {
+            try
+            {
+                List<ComboModel> lista = new List<ComboModel>();
+                ComboModel item;
+                SqlDataReader dr = null;
+                dr = SqlHelper.ExecuteReader(oAuxSQLModel.Conexion, "[dbo].[CFDIFormaPagoDetalle_Combo_sp]");
+                while (dr.Read())
+                {
+                    item = new ComboModel();
+                    item.Id = !dr.IsDBNull(dr.GetOrdinal("id")) ? dr.GetString(dr.GetOrdinal("id")) : string.Empty;
+                    item.Value = !dr.IsDBNull(dr.GetOrdinal("value")) ? dr.GetString(dr.GetOrdinal("value")) : string.Empty;
+                    lista.Add(item);
+                }
+                dr.Close();
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
