9e6458331b66563365e3258791c55658182771a3
CreativaSL <CreativaSL@CREATIVASL-PC>
2018-11-22 10:25:22 -0600

    ok fgg

---
+++ FacturacionSAT.CSL.WEB/Models/ViewModel/FacturaViewModel.cs
@@ -0,0 +1,11 @@
+﻿Fusing System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FacturacionSAT.CSL.WEB.Models.ViewModel
+{
+    public class FacturaViewModel
+    {
+    }
+}
