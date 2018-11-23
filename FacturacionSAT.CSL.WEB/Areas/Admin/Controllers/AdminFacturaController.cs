2c2e7014dc108eeed7a5f2091541f48738ae093d
CreativaSL <CreativaSL@CREATIVASL-PC>
2018-11-23 17:57:44 -0600

    ok fgg

--- FacturacionSAT.CSL.WEB/Areas/Admin/Controllers/AdminFacturaController.cs
+++ FacturacionSAT.CSL.WEB/Areas/Admin/Controllers/AdminFacturaController.cs
@@ -13,22 +13,37 @@ namespace FacturacionSAT.CSL.WEB.Areas.Admin.Controllers
     public class AdminFacturaController : Controller
     {
         private string Conexion = ConfigurationManager.AppSettings.Get("strConnection");
+
         // GET: Admin/Facturacion
         public ActionResult Index()
         {
-            ComboDatos oComboDatos = new ComboDatos();
-            AuxSQLModel oAuxSQLModel = new AuxSQLModel();
-            oAuxSQLModel.Conexion = Conexion;
-            var ListaEmisores = oComboDatos.ListaEmisores(oAuxSQLModel);
+            try
+            {
+                MostrarHTMLError();
+                ComboDatos oComboDatos = new ComboDatos();
+                AuxSQLModel oAuxSQLModel = new AuxSQLModel();
+                oAuxSQLModel.Conexion = Conexion;
+                var ListaEmisores = oComboDatos.ListaEmisores(oAuxSQLModel);
 
-            ViewBag.ListaEmisores = ListaEmisores;
-            return View();
+                ViewBag.ListaEmisores = ListaEmisores;
+
+                return View();
+            }
+            catch (Exception ex)
+            {
+                Error.Success = false;
+                Error.Mensaje = "Se ha generado el siguiente error: \\n" + ex.Message + "\\n Contacte a soporte t&eacute;cnico.";
+                return View();
+            }
+
         }
         [HttpPost]
         public ActionResult Index(IndexFacturaViewModel Model)
         {
             if (!ModelState.IsValid)
             {
+                Error.Success = false;
+                Error.Mensaje = "Verifique sus datos.";
                 return View(Model);
             }
             else
@@ -39,33 +54,80 @@ namespace FacturacionSAT.CSL.WEB.Areas.Admin.Controllers
         [HttpGet]
         public ActionResult Add(IndexFacturaViewModel Model)
         {
-            if(!ModelState.IsValid)
+            try
             {
-                return RedirectToAction("Index", Model);
-            }
-            ComboDatos oComboDatos = new ComboDatos();
-            AuxSQLModel oAuxSQLModel = new AuxSQLModel();
-            oAuxSQLModel.Conexion = Conexion;
+                if (!ModelState.IsValid)
+                {
+                    Error.Success = false;
+                    Error.Mensaje = "Verifique sus datos.";
+                    return RedirectToAction("Index", Model);
+                }
 
-            ViewBag.ListaFormaPago = oComboDatos.ListaFormaPagoDetalle(oAuxSQLModel);
-            ViewBag.ListaMetodoPago = oComboDatos.ListaMetodoPagoDetalle(oAuxSQLModel);
-            ViewBag.ListaMoneda = oComboDatos.ListaMonedaDetalle(oAuxSQLModel);
-            /*
-             * El RFC se divide en 2, personas fisicas y morales, la diferencia:
-             * Morales: Se compone de 3 letras seguidas por 6 dígitos y 3 caracteres alfanumericos = 12
-             * Físicas: consta de 4 letras seguida por 6 dígitos y 3 caracteres alfanuméricos = 13
-            */
-            
-            if (Model.RFCReceptor.Length == 13)
+                ComboDatos oComboDatos = new ComboDatos();
+                AuxSQLModel oAuxSQLModel = new AuxSQLModel();
+                FacturaDatos oFacturaDatos = new FacturaDatos();
+
+                oAuxSQLModel.Conexion = Conexion;
+
+                ViewBag.ListaFormaPago = oComboDatos.ListaFormaPagoDetalle(oAuxSQLModel);
+                ViewBag.ListaMetodoPago = oComboDatos.ListaMetodoPagoDetalle(oAuxSQLModel);
+                ViewBag.ListaMoneda = oComboDatos.ListaMonedaDetalle(oAuxSQLModel);
+                /*
+                 * El RFC se divide en 2, personas fisicas y morales, la diferencia:
+                 * Morales: Se compone de 3 letras seguidas por 6 dígitos y 3 caracteres alfanumericos = 12
+                 * Físicas: consta de 4 letras seguida por 6 dígitos y 3 caracteres alfanuméricos = 13
+                */
+
+                if (Model.RFCReceptor.Length == 13)
+                {
+                    ViewBag.ListaUsoCFDI = oComboDatos.ListaUsoCFDIDetalle(oAuxSQLModel, "78654122-1130-405B-A739-1D19C19955EF");
+                }
+                else
+                {
+                    ViewBag.ListaUsoCFDI = oComboDatos.ListaUsoCFDIDetalle(oAuxSQLModel, "25BD8A08-3C23-4359-8E34-76A2C8E95B3D");
+                }
+
+                FacturacionViewModel oFactura = new FacturacionViewModel();
+                oFactura = oFacturaDatos.Factura_get_Generales_ADD(oAuxSQLModel, Model.CodigoBarra, Model.RFCReceptor, Model.Id_emisor);
+                oFactura.Fecha = DateTime.Today;
+
+                if (!oAuxSQLModel.Success)
+                {
+                     
+                    Error.Success = oAuxSQLModel.Success;
+                    Error.Mensaje = oAuxSQLModel.Mensaje;
+
+                    return RedirectToAction("Index");
+                }
+
+                return View(oFactura);
+            }
+            catch (Exception ex)
             {
-                ViewBag.ListaUsoCFDI = oComboDatos.ListaUsoCFDIDetalle(oAuxSQLModel, "78654122-1130-405B-A739-1D19C19955EF");
+                Error.Success = false;
+                Error.Mensaje = "Se ha generado el siguiente error: \\n" + ex.Message + "\\n Contacte a soporte técnico.";
+                return RedirectToAction("Index");
+            }
+            
+        }
+
+        private void MostrarHTMLError()
+        {
+            if (Error.Success != null || Error.Success == false)
+            {
+                //Guardamos el error en los ViewBag
+                ViewBag.Success = Error.Success;
+                ViewBag.Mensaje = Error.Mensaje;
+
+                //Limpiamos
+                    Error.Success = false;
+                Error.Mensaje = string.Empty;
             }
             else
             {
-                ViewBag.ListaUsoCFDI = oComboDatos.ListaUsoCFDIDetalle(oAuxSQLModel, "25BD8A08-3C23-4359-8E34-76A2C8E95B3D");
+                Error.Success = false;
+                Error.Mensaje = string.Empty;
             }
-
-            return View();
         }
     }
 }
--- FacturacionSAT.CSL.WEB/Models/Datos/JsonDatos.cs
+++ FacturacionSAT.CSL.WEB/Models/Datos/JsonDatos.cs
@@ -33,6 +33,30 @@ namespace FacturacionSAT.CSL.WEB.Models.Datos
                 throw ex;
             }
         }
-        // 
+
+        public List<ComboModel> ListaUnidadMedidaDetalle(AuxSQLModel oAuxSQLModel, string unidadMedidaBuscar)
+        {
+            try
+            {
+                List<ComboModel> lista = new List<ComboModel>();
+                ComboModel item;
+                SqlDataReader dr = null;
+                object[] parametro = { unidadMedidaBuscar };
+                dr = SqlHelper.ExecuteReader(oAuxSQLModel.Conexion, "[dbo].[CFDIClaveUnidadDetalle_Combo_sp_2]", parametro);
+                while (dr.Read())
+                {
+                    item = new ComboModel();
+                    item.Id = !dr.IsDBNull(dr.GetOrdinal("id")) ? dr.GetString(dr.GetOrdinal("id")) : string.Empty;
+                    item.Value = !dr.IsDBNull(dr.GetOrdinal("value")) ? dr.GetString(dr.GetOrdinal("value")) : string.Empty;
+                    lista.Add(item);
+                }
+                dr.Close();
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
     }
 }
