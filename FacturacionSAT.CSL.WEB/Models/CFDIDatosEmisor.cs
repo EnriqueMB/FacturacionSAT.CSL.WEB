4952347507d227b5814919d4f37cac6d2a2e20b2
EnriqueMB <kikeballina1@gmail.com>
2018-11-24 11:51:31 -0600

    Enrique Mendez Ballina

---
+++ FacturacionSAT.CSL.WEB/Models/CFDIDatosEmisor.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FacturacionSAT.CSL.WEB.Models
+{
+    public class CFDIDatosEmisor
+    {
+        private string _IDCFDIDatosEmisor;
+
+        public string IDCFDIDatosEmisor
+        {
+            get { return _IDCFDIDatosEmisor; }
+            set { _IDCFDIDatosEmisor = value; }
+        }
+
+        private string _IDCFDITipoPersona;
+
+        public string IDCFDITipoPersona
+        {
+            get { return _IDCFDITipoPersona; }
+            set { _IDCFDITipoPersona = value; }
+        }
+
+        private string _RazonSocial;
+
+        public string RazonSocial
+        {
+            get { return _RazonSocial; }
+            set { _RazonSocial = value; }
+        }
+
+        private string _RFC;
+
+        public string RFC
+        {
+            get { return _RFC; }
+            set { _RFC = value; }
+        }
+
+        private string _IDCFDIRegimenFiscalDetalle;
+
+        public string IDCFDIReqgimenFiscalDetalle
+        {
+            get { return _IDCFDIRegimenFiscalDetalle; }
+            set { _IDCFDIRegimenFiscalDetalle = value; }
+        }
+
+        private string _Direccion;
+
+        public string Direccion
+        {
+            get { return _Direccion; }
+            set { _Direccion = value; }
+        }
+
+        private string _CodigoPostal;
+
+        public string CodigoPostal
+        {
+            get { return _CodigoPostal; }
+            set { _CodigoPostal = value; }
+        }
+
+        private bool _Predeterminado;
+
+        public bool Predeterminado
+        {
+            get { return _Predeterminado; }
+            set { _Predeterminado = value; }
+        }
+
+        private string _Correo;
+
+        public string Correo
+        {
+            get { return _Correo; }
+            set { _Correo = value; }
+        }
+
+        private string _Password;
+
+        public string Password
+        {
+            get { return _Password; }
+            set { _Password = value; }
+        }
+
+        private string _URLArchivoCER;
+
+        public string URLArchivoCER
+        {
+            get { return _URLArchivoCER; }
+            set { _URLArchivoCER = value; }
+        }
+
+        private string _URLArchivoKEY;
+
+        public string URLArchivoKEY
+        {
+            get { return _URLArchivoKEY; }
+            set { _URLArchivoKEY = value; }
+        }
+
+        private string _PasswordArchivoKEY;
+
+        public string PasswordArchivoKEY
+        {
+            get { return _PasswordArchivoKEY; }
+            set { _PasswordArchivoKEY = value; }
+        }
+
+        private string _Imagen;
+
+        public string Imagen
+        {
+            get { return _Imagen; }
+            set { _Imagen = value; }
+        }
+
+        public string Conexion { get; set; }
+        public string Resultado { get; set; }
+        public string Opcion { get; set; }
+        public string IDUsuario { get; set; }
+    }
+}
---
+++ FacturacionSAT.CSL.WEB/Models/Datos/CFDIDatosEmisorDatos.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FacturacionSAT.CSL.WEB.Models.Datos
+{
+    public class CFDIDatosEmisorDatos
+    {
+        //public CFDIDatosEmisor AbcDatosEmisor(CFDIDatosEmisor)
+        //{
+        //    try
+        //    {
+        //        object[] Parametros = { };
+
+        //    }
+        //    catch (Exception)
+        //    {
+
+        //        throw;
+        //    }
+        //}
+    }
+}
